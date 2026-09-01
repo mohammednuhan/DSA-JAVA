5064470cdb7e4fcb685089c1a8fbe89721a2c54c
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-09-01 19:53:55 +0530

    b added

---
+++ b.java
@@ -0,0 +1,10 @@
+public class b {
+    public static void main (String[]args){
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+        System.out.println("E");
+
+    }
+}

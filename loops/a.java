60fd47343ffdcc23e04fcbbde293665fed6e144a
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-09-01 20:19:40 +0530

    loops

---
+++ loops/a.java
@@ -0,0 +1,17 @@
+package loops;
+
+import java.util.Scanner;
+
+
+public class a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 1;i<=a;i++){
+            System.out.println("bye ");
+        }
+    }
+}
+
+

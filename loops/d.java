1989e482a9fb12d56635c0aeab3297f8ebab60c2
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-09-02 19:38:46 +0530

    d

---
+++ loops/d.java
@@ -0,0 +1,18 @@
+package loops;
+
+import java.util.Scanner;
+
+public class d {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = n; i <= n; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}

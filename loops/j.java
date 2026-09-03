e9fa8a09868e089f8bf97f2dc9b5666523cd4355
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-09-03 19:37:12 +0530

    l,k

---
+++ loops/j.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class j {
+    public static void main(String[] args) {
+       int n = 10 ;
+
+    while (n  < 0){
+
+        System.out.println("n");
+        n--;
+    }
+    System.out.println("printlift");
+
+
+    }
+}
---
+++ loops/k.java
@@ -0,0 +1,16 @@
+package  loops;
+
+import java.util.Scanner;
+
+public class k {
+    public static void main(String []args){
+        
+        Scanner sc = new Scanner(System.in);
+        char ch = 'A';
+
+        while (ch <= 'Z') {
+            System.out.println(ch);
+            ch++;
+        } 
+    }
+}
